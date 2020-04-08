ad2dec9f99b1f52a872ed1bd9411bf25c8379c52
ahuang <mengling915@163.com>
2020-04-08 13:32:28 +0800

    add: 初始化vue工程

---
+++ vue/ts1.button/README.md
@@ -0,0 +1,18 @@
+# ts1.button
+ts实现button组
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
---
+++ vue/ts1.button/src/components/my-btn.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div>
+    my btn
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  computed: {},
+  props: {},
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  methods: {
+  }
+}
+</script>
+<style scoped>
+  
+</style>
